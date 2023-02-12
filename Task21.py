1623e319d6448b30d5ab477850b00f5f77734f0b
Natalya_Lazar <nata@list.ru>
2023-02-12 11:53:54 +0300

    12/02/23

--- Task21.py
+++ Task21.py
@@ -9,5 +9,22 @@ my_list = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":
 my_list1 = set()
 for i in my_list:
     for value in i.values():
-        my_list1.add(value)
-print(*my_list1)        
+        my_list1.add(value.strip())
+print(*my_list1) 
+
+# Еще одно решение:
+
+input_list = [{"V": " S001"}, {"V": " S002"}, {"VI": "S001 "},
+{"VI": "S005 "}, {"VII": " S005"}, {" V ":"S009"}, {" VIII":"S007 "}]
+
+unic_values = set()
+for d in input_list:
+print('d это - ',d)
+print('d.values это -', d.values())
+elem = d.values()
+elem = tuple(elem)
+elem = elem[0]
+elem = elem.strip()
+unic_values.add(elem)
+
+print(unic_values)
--- Task23.py
+++ Task23.py
@@ -5,6 +5,25 @@
 # Input: [0, -1, 5, 2, 3]
 # Output: 2 (-1 < 5, 2 < 3)
 
+Решение 1:
+from random import randint
+my_list = [randint(-10, 10) for _ in range(randint(1, 10))]
+
+counter = 0
+for i in range(1, len(my_list)):
+    if my_list[i] > my_list[i - 1]:
+        counter += 1
+print(counter)
+
+Решение 2:
+from random import randint
+my_list = [randint(-10, 10) for _ in range(randint(1, 10))]
+
+new_list = [1 for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
+print(len(new_list))
+
+
+Решение 3:
 def count_larger_then_previous(arr):
     count = 0
     for i in range(1, len(arr)):
