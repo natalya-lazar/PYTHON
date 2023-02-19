b96014e283fcd0d128aaf28f2aa7fd9ba20daace
Natalya_Lazar <nata@list.ru>
2023-02-19 21:51:36 +0300

    HW 6

--- Task32.py
+++ Task32.py
@@ -7,3 +7,7 @@
 # 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
 
+from random import randint
+start, stop = [int(i) for i in input("Enter range: ").split()]
+list_num = [randint(-10, 10) for i in range(randint(10, 20))]
+print(list_num, [i for i, n in enumerate(list_num) if n in range(start, stop + 1)], sep="\n")
