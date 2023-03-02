2e166cbef0182e1e3e4c330e77ea48c7ca83cf80
Natalya_Lazar <nata@list.ru>
2023-03-02 21:42:16 +0300

    HW pb

--- Task49_1.py
+++ Task49_1.py
@@ -3,79 +3,67 @@
 # формате .txt. Фамилия, имя, отчество, номер
 # телефона - данные, которые должны находиться
 # в файле.
-# 1.Программа должна выводить данные
-# 2.Программа должна сохранять данные в текстовом файле
-# 3.Пользователь может ввести одну из характеристик для
-# поиска определенной записи(Например имя или фамилию человека)
-# 4.Использование функций. Ваша программа не должна быть линейной
+# 1. Программа должна выводить данные
+# 2. Программа должна сохранять данные в
+# текстовом файле
+# 3. Пользователь может ввести одну из
+# характеристик для поиска определенной
+# записи(Например имя или фамилию
+# человека)
+# 4. Использование функций. Ваша программа
+# не должна быть линейной
 
-# модули
-# импорт данных в TXT
-# экспорт данных в TXT
-# внесение записи через консоль
-# изменение и удаление записи через консоль
-# поиск записи по любому вхождению
-# user interface
-
-# формат словаря:
-# id surname name fathers_name tel
-
-def main_import_contacts():
-    with open('phonebook.txt', 'r', encoding='utf-8') as data:
-        s = data.readlines()
-        for i in range(len(s)):
-            phonebook[i] = s[i].split()
-        # print(phonebook)
-
-def import_contacts(some_string):
-    finded_contacts = list()
-    for i in phonebook:
-        if some_string in phonebook[i]:
-            finded_contacts.append(phonebook[i])
-    return finded_contacts        
+def print_records(file_name: str):
+    with open(file_name, 'r', encoding='utf-8') as data:
+        for line in data:
+            print(*line.split(';'))
 
 
-def export_contacts(new_contact):
-    with open('phonebook.txt', 'a+', encoding='utf-8') as data:
-        # s = ' '.join(new_line)
-        data.writelines(' '.join(new_contact) + '\n')
-    
-
-def input_contacts():
-    new_contact = [input('surname: ')]
-    new_contact.append(input('name: '))
-    new_contact.append(input('given name: '))
-    new_contact.append(input('phone number: '))
-    
-    export_contacts(new_contact)
-
-def find_contact():
-    s = import_contacts(input('whadda we search?: '))
-    print(*s)
-
-def user_interface():
-    main_import_contacts()
-    print('phonebook\nwhadda want?\n1 - add contact\n2 - find contact\n3 - print whole book\nany other input - end program')
-    user_input = input('your choice: ')
-    while user_input in ('1', '2', '3'):
-        if user_input == 1:
-            input_contacts()
-        elif user_input == '2':
-            find_contact()
-        elif user_input == '3':
-            print()
-            for i in phonebook:
-                print(*phonebook[i])
-        user_input = input('\nyour choice: ')
-
-    print('bye')          
+def input_records(file_name: str):
+    with open(file_name, 'r+', encoding='utf-8') as data:
+        record_id = 0
+        for line in data:
+            if line != '':
+                record_id = line.split(';')[:4][0]
+        print('Введите фамилию, имя, отчество, номер телефона через пробел')
+        line = f'{int(record_id) + 1};' + ';'.join(input().split()[:4]) + ';\n'
+        data.write(line)
 
 
-phonebook = dict()
-user_interface()
+def find_records(file_name: str):
+    print('По какой характеристике искать?')
+    print('1 - фамилия, 2 - имя, 3 - отчество, 4 - номер')
+    characteristic = input()
+    while characteristic not in ['1', '2', '3', '4']:
+        print('Введены неверные данные')
+        print('По какой характеристике искать?')
+        print('1 - фамилия, 2 - имя, 3 - отчество, 4 - номер')
+        characteristic = input()
+    condition = input('Введите поисковое значение\n')
+    printed = False
+    with open(file_name, 'r', encoding='utf-8') as data:
+        for line in data:
+            if condition == line.split(';')[int(characteristic)]:
+                print(*line.split(';'))
+                printed = True
+    if not printed:
+        print("Не найдено")
 
-    
 
+path = 'phone_book.txt'
 
-
-
+action = None
+while action != 'q':
+    action = input('Какое действие хотите совершить? 1 - чтение, 2 - запись. 3 - поиск, q - выход\n')
+    while action not in ['1', '2', '3', 'q']:
+        action = input('Какое действие хотите совершить? 1 - чтение, 2 - запись. 3 - поиск, q - выход\n')
+        if action not in ['1', '2', '3', 'q']:
+            print('Введены неверные данные')
+    if action != 'q':
+        if action == '1':
+            print_records(path)
+        elif action == '2':
+            input_records(path)
+        else:
+            find_records(path)
+            
